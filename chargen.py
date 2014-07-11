133dc4242a2253b74d83d19baba9f2c6062ef967
zaboron <csiebenkaes@gmail.com>
2014-07-11 17:22:28 +0200

    more testing

--- chargen.py
+++ chargen.py
@@ -1,45 +1,46 @@
 from tkinter import *
 import tkinter.messagebox as MessageBox
+from collections import namedtuple
+from operator import itemgetter
 
+Rasse = namedtuple('Rasse', 'name Beschreibung')
+Rassen = [Rasse('Alb','Ein Elf'),  Rasse('Gnom','Ein Gnom'), Rasse('Varg','Ein Wolf'), Rasse('Mensch','Ein Mensch')]
 
 class App(Toplevel):
-    def __init__(self,master):
+    def __init__(self,master, entries):
         super(App, self).__init__(master)#this is Python 3, don't know about Python 2
-        self.Rassen = [Rasse('Alb','Ein Elf'),  Rasse('Gnom','Ein Gnom'), Rasse('Varg','Ein Wolf')]
-        self.zahl_var = StringVar()#you should google "tkinter variables" if you don't know about these yet
+        self.entries = entries
+        self.zahl_var = StringVar() #you should google "tkinter variables" if you don't know about these yet
+        self.zahl_var.set("You selected the option " + self.entries[0].Beschreibung)
+        self.buttonwidth = max(len(entry.name) for entry in entries)
         self.generiereButtons()
-        
    
         
     def generiereButtons(self):
         button_frame = LabelFrame(self, text='Click a button!')
         label = Label(button_frame,textvariable = self.zahl_var)
-        for element in self.Rassen:  
-            Radiobutton(button_frame, text=element.name,  variable=self.zahl_var, value="You selected the option " + element.name,
-                     command=self.sel(element, label),indicatoron=0).pack( anchor = W )
-        
+        for element in self.entries:  
+            Radiobutton(button_frame, text=element.name,  variable=self.zahl_var, value="You selected the option " + element.Beschreibung,
+                     command=self.sel(),indicatoron=0, width = self.buttonwidth).pack( anchor = W )
         label.pack()
         button_frame.pack()
-        self.sel(self.Rassen[1],label)
+        self.sel()
         
-    def sel(self, element, label):
-        selection = "You selected the option " + element.Beschreibung
-        self.zahl_var.set(selection)
-        label.config(text = self.zahl_var) 
-        MessageBox.showinfo(message='You selected '+selection)
+    def sel(self):
+        selection = self.zahl_var.get()
         #root.update_idletasks()
-
+        
+class Rasse(App):
+     def sel(self):
+        selection = self.zahl_var.get()
+        #root.update_idletasks()
     
-class Rasse():
-    def __init__(self, name, Beschreibung):
-        self.name = name
-        self.Beschreibung = Beschreibung
         
 
 if __name__ == '__main__':
 
     root = Tk()
-    app = App(root)
+    app = App(root, Rassen)
     root.mainloop()
 
 
--- test2.py
+++ test2.py
@@ -7,17 +7,21 @@ class App(Toplevel):
         super(App, self).__init__(master)#this is Python 3, don't know about Python 2
         self.elements = [Zahl('Eins','Eine Eins'), Zahl('Zwei','Eine Zwei'), Zahl('Drei','Eine Drei')]
         self.zahl_var = StringVar()#you should google "tkinter variables" if you don't know about these yet
+        self.zahl_var.set(self.elements[0].Beschreibung)
         self.generiereButtons()
 
     def generiereButtons(self):
         button_frame = LabelFrame(self, text='Click a button!')
+        label = Label(button_frame,textvariable = self.zahl_var)
         for element in self.elements:
-            r = Radiobutton(button_frame, text=element.name, variable=self.zahl_var, value=element.Beschreibung, command=self.show_selection)
+            r = Radiobutton(button_frame, text=element.name, variable=self.zahl_var, value=element.Beschreibung, command=self.show_selection(element,label))
             r.pack()
+        label.pack()
         button_frame.pack()
 
-    def show_selection(self):
+    def show_selection(self, element, label):
         selection = self.zahl_var.get()
+        label.config(text = selection) 
         MessageBox.showinfo(message='You selected '+selection)
 
 
